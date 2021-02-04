98ade7eaae5e644f719f0893786e78e305e38e31
nicoloddo <gnicoloddo@gmail.com>
2021-02-04 19:17:32 +0100

    Prima implementazione di get_clusters

--- INTERFACE/PORTAMENTO_unity/Assets/interface_scripts/get_clusters.cs
+++ INTERFACE/PORTAMENTO_unity/Assets/interface_scripts/get_clusters.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class get_clusters : MonoBehaviour
 {
-    private static string PATHS_FILE_NAME = "last_path.csv";
+    private static string PATHS_FILE_NAME = @"\last_path.csv";
 
     private string current_path = System.IO.Directory.GetCurrentDirectory();
     public string base_path;
@@ -14,7 +14,10 @@ public class get_clusters : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        base_path = current_path.Remove(current_path.Length - 52);
+        base_path = current_path.Remove(current_path.Length - 27);  // 27 PERCHE' QUESTO E' IL NUMERO DI CARATTERI DA CANCELLARE PER OTTENERE IL base_path
+        string last_path_file = string.Concat(base_path, PATHS_FILE_NAME);
+
+        paths = strings_csv_to_dict(last_path_file);
         
     }
 
@@ -24,10 +27,12 @@ public class get_clusters : MonoBehaviour
         
     }
 
-    private Dictionary<string, string> string_keys_csv_to_dict(string path_csv)
+    private Dictionary<string, string> strings_csv_to_dict(string path_csv)
     {
         Dictionary<string, string> dict = new Dictionary<string, string>();
-        string Data = System.IO.File.ReadAllText(path_csv);
+        string file_data = System.IO.File.ReadAllText(path_csv);
+        string[] keys = file_data.Split('\n')[0].Split(',');
+        
 
         return dict;
     }
