5161e656117509e9046f601a381fffa1cf86b335
nicoloddo <gnicoloddo@gmail.com>
2021-02-05 18:49:54 +0100

    CharacterController e animazioni

---
+++ INTERFACE/PORTAMENTO_unity/Assets/Player/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // 
+    private Animator animator;
+    private CharacterController characterController;
+
+    // INPUTS
+    private float inputUpward; // Per muoversi verso sù
+    private float inputVertical; //Tasti per muoversi in avanti
+    private float inputHorizontal; //Tasti per ruotare (oltre al mouse)
+    private float totalXRot; //Rotazione in X totale
+    private float totalYRot; //Rotazione in Y totale
+
+    // PARAMETRI
+    private float rotateSpeedX = 4f; //Velocita' di rotazione X
+    private float rotateSpeedY = 3f; //Velocita' di rotazione Y
+    public float forwardSpeed = 0.5f; // Velocità di avanzamento
+    public float upwardSpeed = 0.3f; // Velocità di levitazione
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        inputVertical = Input.GetAxis("Vertical");
+        inputHorizontal = Input.GetAxis("Horizontal"); 
+        inputUpward = Input.GetAxis("Jump");
+
+        animator.SetFloat("fly", inputVertical);
+
+        // Controller della rotazione, è fatto così per evitare la rotazione in z, che può avvenire per somma di rotazioni in x e y
+        totalXRot += Input.GetAxis("Mouse X") * rotateSpeedX;
+        totalXRot += inputHorizontal * rotateSpeedX;
+        totalYRot -= Input.GetAxis("Mouse Y") * rotateSpeedY;
+        transform.rotation = Quaternion.Euler(totalYRot, totalXRot, 0f);
+
+        // Movimento
+        characterController.Move(transform.forward * inputVertical * forwardSpeed);
+        characterController.Move(transform.up * inputUpward * upwardSpeed);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        
+    }
+
+
+
+}
