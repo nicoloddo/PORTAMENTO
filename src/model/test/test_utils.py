c2712bdc3393afc8fb252896b7ce8ebb0e03cb1d
nicoloddo <gnicoloddo@gmail.com>
2023-12-19 19:40:07 +0100

    Clarified and generalized print_songs functions

--- src/model/test/test_utils.py
+++ src/model/test/test_utils.py
@@ -21,17 +21,30 @@ def make_test_results_folder(folder_path):
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
         
-def print_songs(samples, dataset):
+def print_songs_from_idlist(samples, dataset, max_print = 5):
     names = ""
     for i, sample in enumerate(samples):
         names += dataset.loc[sample]['name']
         if i == len(samples) -1: # We reached the last sample, let's not add any punctuation
             break
-        if i >= 5:
+        if i >= max_print:
             names += '...'
             break
         else: names += ', '
     print(names)
+    
+def print_songs_from_df(samples, max_print = 5):
+    # Extract the top 5 names from the 'samples' DataFrame
+    top_names = samples['name'].head(5).tolist()  # Replace 'name' with the actual column name
+    
+    # Check if the DataFrame has more than 5 rows
+    if len(samples) > max_print:
+        top_names_string = ', '.join(top_names) + ', ...'
+    else:
+        top_names_string = ', '.join(top_names)
+    
+    print(top_names_string)
+
 
 #****************************************************************************
 if running_in_docker():
