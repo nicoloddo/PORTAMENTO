5a77a55fb297bc45e5903811bbf516e54445d6a3
nicoloddo <gnicoloddo@gmail.com>
2023-12-20 13:18:00 +0100

    More extensive song fetcher tests

--- src/model/cloud_services/test/aws/spotify_fetch/song_fetcher/test_song_fetcher.ps1
+++ src/model/cloud_services/test/aws/spotify_fetch/song_fetcher/test_song_fetcher.ps1
@@ -1,1 +1,2 @@
-sam local invoke "PortamentoSongFetcher" --event mock_sqs_event.json -t ../template.yml
+sam local invoke "PortamentoSongFetcher" --event mock_sqs_event1.json -t ../template.yml
+sam local invoke "PortamentoSongFetcher" --event mock_sqs_event2.json -t ../template.yml
