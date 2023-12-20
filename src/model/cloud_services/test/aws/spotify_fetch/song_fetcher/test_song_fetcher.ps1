34bd28550d5757d7786ef04b2937a7fd2a2591c6
nicoloddo <gnicoloddo@gmail.com>
2023-12-20 14:44:12 +0100

    Full 4 playlist test (mosiselecta test)

--- src/model/cloud_services/test/aws/spotify_fetch/song_fetcher/test_song_fetcher.ps1
+++ src/model/cloud_services/test/aws/spotify_fetch/song_fetcher/test_song_fetcher.ps1
@@ -1,2 +1,4 @@
 sam local invoke "PortamentoSongFetcher" --event mock_sqs_event1.json -t ../template.yml
 sam local invoke "PortamentoSongFetcher" --event mock_sqs_event2.json -t ../template.yml
+sam local invoke "PortamentoSongFetcher" --event mock_sqs_event3.json -t ../template.yml
+sam local invoke "PortamentoSongFetcher" --event mock_sqs_event4.json -t ../template.yml
