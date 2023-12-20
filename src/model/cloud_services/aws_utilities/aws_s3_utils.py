c1cc2416d0cb11ff142306f477c7610a49e8f639
nicoloddo <gnicoloddo@gmail.com>
2023-12-20 01:16:02 +0100

    New S3 functionalities added

--- src/model/cloud_services/aws_utilities/aws_s3_utils.py
+++ src/model/cloud_services/aws_utilities/aws_s3_utils.py
@@ -42,7 +42,7 @@ def save_to_s3(data, file_name, endpoint_url=ENDPOINT_URL, bucket_name=S3_BUCKET
         print(f"An error occurred: {e}")
 
 
-def read_file_from_s3(bucket, key, endpoint_url=ENDPOINT_URL):
+def read_file_from_s3(key, endpoint_url=ENDPOINT_URL, bucket_name=S3_BUCKET_NAME):
     """
     Reads a file from an S3 bucket and returns its content. It works with a specified
     endpoint URL (e.g., LocalStack) or defaults to AWS S3 if no endpoint URL is provided.
@@ -62,12 +62,84 @@ def read_file_from_s3(bucket, key, endpoint_url=ENDPOINT_URL):
 
     try:
         # Retrieve the file from the specified bucket and key
-        response = s3.get_object(Bucket=bucket, Key=key)
+        response = s3.get_object(Bucket=bucket_name, Key=key)
         
         # Read and return the content of the file
         return response['Body'].read().decode('utf-8')
     except (BotoCoreError, ClientError) as e:
         print(f"An error occurred: {e}")
+        return None
+
+def list_folder_files_s3(prefix, endpoint_url=ENDPOINT_URL, bucket_name=S3_BUCKET_NAME):
+    """
+    Lists all files in a specified folder in an S3 bucket.
+
+    :param prefix: Prefix of the folder to list files from in the S3 bucket.
+    :param bucket_name: Name of the S3 bucket.
+    :return: A response object containing the listed files under the specified prefix.
+    """
+    # Create an S3 client
+    if endpoint_url:
+        # Use the specified endpoint URL
+        s3 = boto3.client('s3', endpoint_url=endpoint_url)
+    else:
+        # Default to AWS S3
+        s3 = boto3.client('s3')
+        
+    try:
+        # List objects under the specified prefix in the bucket
+        response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+
+        return response
+    except (BotoCoreError, ClientError) as e:
+        print(f"An error occurred while listing files: {e}")
+        return {'Contents': []}  # Return an empty list in case of an error
+    
+def delete_folder(prefix, endpoint_url=ENDPOINT_URL, bucket_name=S3_BUCKET_NAME):
+    """
+    Deletes a folder (and its contents) in an S3 bucket. This function removes all 
+    objects under the specified prefix.
+
+    :param bucket: Name of the S3 bucket.
+    :param prefix: Prefix of the folder to delete in the S3 bucket.
+    """
+    # Create an S3 client
+    if endpoint_url:
+        # Use the specified endpoint URL
+        s3 = boto3.resource('s3', endpoint_url=endpoint_url)
+    else:
+        # Default to AWS S3
+        s3 = boto3.resource('s3')
+        
+    bucket = s3.Bucket(bucket_name)
+    try:
+        # Delete all objects under the specified prefix
+        bucket.objects.filter(Prefix=prefix).delete()
+    except (BotoCoreError, ClientError) as e:
+        print(f"An error occurred while deleting folder: {e}")
+
+def get_database(database_key="database.csv", bucket_name=S3_BUCKET_NAME):
+    """
+    Retrieves the CSV file in the given S3 bucket that contains the main database.
+
+    :param bucket_name: Name of the S3 bucket.
+    :param database_key: Key of the CSV file in the S3 bucket.
+    :return: The Database CSV file or None if an error occurs.
+    """
+    s3_client = boto3.client('s3')
+    try:
+        # Retrieve the CSV file from the S3 bucket
+        return s3_client.get_object(Bucket=bucket_name, Key=database_key)
+
+    except ClientError as e:
+        error_code = e.response['Error']['Code']
+        if error_code == 'NoSuchKey':
+            print(f"File not found: {database_key} does not exist in the bucket {bucket_name}.")
+        else:
+            print(f"An error occurred while reading the CSV file: {e}")
+        return None
+    except BotoCoreError as e:
+        print(f"An AWS error occurred: {e}")
         return None
 
 def check_s3():
