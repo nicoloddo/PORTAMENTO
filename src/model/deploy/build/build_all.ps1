bad51120557a1c1893b10e0dce427dfc565ed6dc
nicoloddo <gnicoloddo@gmail.com>
2024-02-07 18:01:25 +0100

    Moved the container builders in a deploy folder

--- src/model/deploy/build/build_all.ps1
+++ src/model/deploy/build/build_all.ps1
@@ -1,8 +1,8 @@
-$scriptDirectory = ".\"
+$scriptsDirectory = ".\"
 $selfScriptName = $MyInvocation.MyCommand.Name # Get the name of the current script
 
 # Get all .ps1 files in the directory
-$scriptFiles = Get-ChildItem -Path $scriptDirectory -Filter *.ps1
+$scriptFiles = Get-ChildItem -Path $scriptsDirectory -Filter *.ps1
 
 # Loop through each file and open it in a new PowerShell window, excluding the current script
 foreach ($file in $scriptFiles) {
---
+++ src/model/deploy/build/build_song_fetcher_container.ps1
@@ -0,0 +1,1 @@
+docker build -t portamento_song_fetcher ../../
