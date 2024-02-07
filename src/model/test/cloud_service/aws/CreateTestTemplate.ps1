8e54e5cba34e7b6a54a90024e0168549f5408ce6
nicoloddo <gnicoloddo@gmail.com>
2024-02-07 18:33:27 +0100

    New test creation template adapts better to the improved modularity of the original template

--- src/model/test/cloud_service/aws/CreateTestTemplate.ps1
+++ src/model/test/cloud_service/aws/CreateTestTemplate.ps1
@@ -3,11 +3,29 @@ param (
     [string]$newTemplatePath
 )
 
+# Define the LocalStack replacements
+$fetchQueueUrlReplacement = '"http://sqs.eu-west-3.localhost.localstack.cloud:4566/000000000000/portamento-fetch-queue"'
+$mergeQueueUrlReplacement = '"http://sqs.eu-west-3.localhost.localstack.cloud:4566/000000000000/portamento-merge-queue"'
+$endpointUrlReplacement = '"http://host.docker.internal:4566"'
+$roleReplacement = 'Role: arn:aws:iam::000000000000:role/lambda-ex'
+
 # Read the original template
 $content = Get-Content $originalTemplatePath -Raw
 
 # Modify the ImageUri values
 $modifiedContent = $content -replace 'ImageUri: .*/', 'ImageUri: '
+
+# Modify the Role values
+$modifiedContent = $modifiedContent -replace 'Role: .*', $roleReplacement
+
+# Replace the CloudFormation references with LocalStack values
+$modifiedContent = $modifiedContent -replace '!GetAtt PortamentoFetchQueue.QueueUrl', $fetchQueueUrlReplacement
+$modifiedContent = $modifiedContent -replace '!GetAtt PortamentoMergeQueue.QueueUrl', $mergeQueueUrlReplacement
+
+# Add the ENDPOINT_URL to the Globals section
+$globalsPattern = '(Globals:\s+Function:\s+Environment:\s+Variables:)'
+$endpointInsertion = "`$1`n        ENDPOINT_URL: $endpointUrlReplacement"
+$modifiedContent = $modifiedContent -replace $globalsPattern, $endpointInsertion
 
 # Write the modified content to the new template
 Set-Content $newTemplatePath $modifiedContent
