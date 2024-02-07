6196cc835977039fcbccfd35fd96fa58ff7bda02
nicoloddo <gnicoloddo@gmail.com>
2024-02-07 22:24:52 +0100

    Added creation of repository and docker login

--- src/model/deploy/TagAndPush.ps1
+++ src/model/deploy/TagAndPush.ps1
@@ -8,6 +8,9 @@ $templateFilePath = "..\template.yml"
 # Read the contents of the template.yml file
 $templateFileContent = Get-Content -Path $templateFilePath
 
+# Login to docker-aws
+aws ecr get-login-password --region $awsRegion | docker login --username AWS --password-stdin "$awsAccountId.dkr.ecr.$awsRegion.amazonaws.com"
+
 # Process each line to find and handle ImageUri entries
 foreach ($line in $templateFileContent) {
     if ($line.Trim().StartsWith("ImageUri:")) {
@@ -17,12 +20,24 @@ foreach ($line in $templateFileContent) {
         # Extract just the image name (repository and tag) from the full ECR image name
         $imageName = $fullEcrImageName -replace "$awsAccountId\.dkr\.ecr\.$awsRegion\.amazonaws\.com\/", ""
 
+        # Extract the repository name from the image name
+        $repositoryName = $imageName.Split(":")[0]
+
         # Print the image names
         Write-Host "Local Image Name: $imageName"
         Write-Host "Full ECR Image Name: $fullEcrImageName"
+        Write-Host "Repository Name: $repositoryName"
 
         # Wait for user confirmation to continue
         Read-Host -Prompt "Press Enter to continue or Ctrl+C to abort"
+
+        # Check if the repository exists
+        $repoExists = aws ecr describe-repositories --region $awsRegion --repository-names $repositoryName 2>&1
+        if ($repoExists -like "*RepositoryNotFoundException*") {
+            # Create repository if it doesn't exist
+            Write-Host "Creating repository: $repositoryName"
+            aws ecr create-repository --repository-name $repositoryName --region $awsRegion
+        }
 
         # Retagging the local image with the ECR image name
         docker tag $imageName $fullEcrImageName
