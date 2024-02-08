ac2806b8c121d404ded1c3b96b94915b7fe566ba
nicoloddo <gnicoloddo@gmail.com>
2024-02-08 13:02:20 +0100

    Test complies to the new body format

--- src/model/test/api/test_api.py
+++ src/model/test/api/test_api.py
@@ -20,23 +20,10 @@ headers = {
 }
 
 # Example data
-data = "spotify:playlist:5T54YmcS3PsSghWrqivfms\r\nspotify:playlist:7460eJSzkWMB3VQ2xzACiV\r\nspotify:playlist:2HF1k7XYrLeooG2EYzIYwk\r\nspotify:playlist:7rldaIGrPgeiOVcfeQ5ZjR"
+data = "spotify:playlist:5T54YmcS3PsSghWrqivfms,spotify:playlist:7460eJSzkWMB3VQ2xzACiV,spotify:playlist:2HF1k7XYrLeooG2EYzIYwk,spotify:playlist:7rldaIGrPgeiOVcfeQ5ZjR"
 
 response = requests.post(url, headers=headers, data=json.dumps(data))
 
 # This will print the status code and response body
 print(f'Status Code: {response.status_code}')
 print(f'Response: {response.json()}')
-
-
-'''
-This is what was received:
-{"request_id": "52a34ac4-44cc-4431-9032-3f85e5ef05e9-1707351206", 
- "req_n_playlists": 1, 
- "playlist_uri": "\"\\nspotify:playlist:5T54YmcS3PsSghWrqivfms\\r\\nspotify:playlist:7460eJSzkWMB3VQ2xzACiV\\r\\nspotify:playlist:2HF1k7XYrLeooG2EYzIYwk\\r\\nspotify:playlist:7rldaIGrPgeiOVcfeQ5ZjR\\n\"", 
- "start_index": 0, "batch_size": 100}
-
-One only playlist_uri with wrongly parsed \n and \r
-
-Moreover, the SQS was continuously triggering 4 song fetchers all the time.
-'''
