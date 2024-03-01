2701a7c13a2b4a2e237353ceb261a2736e867cae
nicoloddo <gnicoloddo@gmail.com>
2024-03-01 01:14:33 +0100

    Removed the S3 triggered playlist_processor as we passed to an API trigger

--- src/model/test/cloud_service/aws/spotify_fetch/playlist_processor_s3_trigger/s3_txt_upload.ps1
+++
@@ -1,1 +0,0 @@
-aws --endpoint-url=http://localhost:4566 s3 cp ./mosiselecta.txt s3://portamento-bucket/mosiselecta.txt
--- src/model/test/cloud_service/aws/spotify_fetch/playlist_processor_s3_trigger/test_playlist_processor.ps1
+++
@@ -1,1 +0,0 @@
-sam local invoke "PortamentoPlaylistProcessor" --event mock_s3_event.json -t $TEMPLATE_PATH
