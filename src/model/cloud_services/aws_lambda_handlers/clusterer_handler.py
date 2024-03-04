cef76465d88360ce6ee3ead2d767966bbafc0fc9
nicoloddo <gnicoloddo@gmail.com>
2024-03-04 13:58:22 +0100

    The clusterer handler now properly read the files returned by S3

--- src/model/cloud_services/aws_lambda_handlers/clusterer_handler.py
+++ src/model/cloud_services/aws_lambda_handlers/clusterer_handler.py
@@ -12,9 +12,11 @@ and start the clustering.
 """
 
 from cloud_services.aws_utilities.aws_s3_utils import read_file_from_s3, save_to_s3
+from core.clusterer import Clusterer
+
 import json
 import pickle
-from core.clusterer import Clusterer
+import pandas as pd
 
 def save_model_callback(model, path):
     # Serialize the model to bytes
@@ -39,11 +41,13 @@ def lambda_handler(event, context):
         config['model_path'] = f'{data_id}/model.pkl'
 
     data_key = f'{data_id}/data.csv'
-    dataset = read_file_from_s3(data_key)
+    dataset_file = read_file_from_s3(data_key)
+    dataset = pd.read_csv(dataset_file)
     
     if base_model_id is not None: # Initialize with a base model from file
         base_model_key = f'{base_model_id}/model.pkl'
-        base_model = read_file_from_s3(base_model_key)
+        base_model_file = read_file_from_s3(base_model_key)
+        base_model = pickle.load(base_model_file)
         clusterer = Clusterer(config, base_model, save_callback = save_model_callback)
         
     else: # Initialize without base model
