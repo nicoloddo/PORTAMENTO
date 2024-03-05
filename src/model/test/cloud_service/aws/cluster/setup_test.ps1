a7cf1bd5d77a371825574d2ed0a94e20cc1aece0
nicoloddo <gnicoloddo@gmail.com>
2024-03-05 20:04:16 +0100

    The cluster tests now build a more close to reality environment

--- src/model/test/cloud_service/aws/cluster/setup_test.ps1
+++ src/model/test/cloud_service/aws/cluster/setup_test.ps1
@@ -8,13 +8,18 @@ aws --endpoint-url=http://localhost:4566 s3 mb s3://portamento-bucket
 aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name portamento-fetch-queue
 aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name portamento-merge-queue
 
-# Load the data.csv to try the plain clusterer
-$key = "34ec81b9-33be-4ff4-95c8-262f22957ed6-1703073359/data.csv"
-aws s3 cp ./data.csv s3://portamento-bucket/$key --endpoint-url=http://localhost:4566
+# To try the full clusterer:
+# Load the full data.csv
+aws s3 cp ./data.csv s3://portamento-bucket/full-clusterer-test/data.csv --endpoint-url=http://localhost:4566
 
-# Load the half_model.pkl to try the online learning clusterer
-aws s3 cp ./second_half_data.csv s3://portamento-bucket/online-learning-test/data.csv --endpoint-url=http://localhost:4566
+# To try the online learning clusterer:
+# Load the half_model.pkl and its data to the default folder 
 aws s3 cp ./half_model.pkl s3://portamento-bucket/default/model.pkl --endpoint-url=http://localhost:4566
+aws s3 cp ./first_half_data.csv s3://portamento-bucket/default/data.csv --endpoint-url=http://localhost:4566
+
+# Load the second half of data
+aws s3 cp ./second_half_data.csv s3://portamento-bucket/online-learning-test/data.csv --endpoint-url=http://localhost:4566
+
 
 # Set the template path and start the testing shell
 
