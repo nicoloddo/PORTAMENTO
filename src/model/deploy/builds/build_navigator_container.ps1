624a93baa226268b9383e1bac3d9159a1a3837f7
nicoloddo <gnicoloddo@gmail.com>
2024-03-05 20:45:50 +0100

    Fixed the requirements of the navigator by adding the clustering tree ones
    The navigator calls the clusterer when loading the pickle, therefore it needs the clustering requirements as well

--- src/model/deploy/builds/build_navigator_container.ps1
+++ src/model/deploy/builds/build_navigator_container.ps1
@@ -1,1 +1,1 @@
-docker build --build-arg REQUIREMENTS_FILE=requirements_nav.txt -t portamento_navigator ../../
+docker build --build-arg REQUIREMENTS_FILE=requirements_cluster.txt -t portamento_navigator ../../
