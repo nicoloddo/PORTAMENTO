4d9369c4b92d0afa9b1672800f3b8d509ac390a1
nicoloddo <gnicoloddo@gmail.com>
2024-10-25 17:29:45 +0200

    Updated environment to most recent setup

--- src/model/test/python/test_navigator_json.py
+++ src/model/test/python/test_navigator_json.py
@@ -40,61 +40,56 @@ navigator = BirchTreeNavigator(loaded_model)
 # Start the navigation
 current_node_id = "0"
 while True:
-    try:
-        node_json = navigator.get_node(current_node_id).to_json(dataset, columns_blacklist=["artists", "album"]) # This line gets the json
-        current_node = json.loads(node_json) # Transform into python dict
+    node_json = navigator.get_node(current_node_id).to_json(dataset, columns_blacklist=["artists", "album"]) # This line gets the json
+    current_node = json.loads(node_json) # Transform into python dict
+    
+    # Print Node information and collect input from user on the next node to navigate to
+    if current_node['is_leaf']: # Node is leaf, only print the songs of the node
+        print("This node is a leaf node and has no further children.")
+        samples = pd.DataFrame.from_dict(current_node['samples'], orient='index')
+        print(f"These are its {len(samples)} songs (max 5 are displayed):")
+        test_utils.print_songs_from_df(samples)
         
-        # Print Node information and collect input from user on the next node to navigate to
-        if current_node['is_leaf']: # Node is leaf, only print the songs of the node
-            print("This node is a leaf node and has no further children.")
-            samples = pd.DataFrame.from_dict(current_node['samples'], orient='index')
-            print(f"These are its {len(samples)} songs (max 5 are displayed):")
-            test_utils.print_songs_from_df(samples)
+        # Get user input for what to do
+        user_input = input("Enter '-' to go to the parent node (or enter 'exit' to quit): ") 
+    
+    else: # Node is not leaf, let's print its children
+        print()
+        print('---------------------------------------')
+        print(f"Current node ID is '{current_node_id}'. These are its children and their songs:")
+        for i in range(current_node['n_children']):
             
-            # Get user input for what to do
-            user_input = input("Enter '-' to go to the parent node (or enter 'exit' to quit): ") 
-        
-        else: # Node is not leaf, let's print its children
+            print()
+            if current_node['children'][i]['is_leaf']:
+                print(f"{i}: Child {i} (leaf)")
+            else:
+                print(f"{i}: Child {i}")
+            
+            # And let's print the songs of each children
+            child_samples = pd.DataFrame.from_dict(current_node['children'][i]['samples'], orient='index')
+            print(f"With {len(child_samples)} songs, among which:")
+            test_utils.print_songs_from_df(child_samples)
+
+        # Get user input for the next node to navigate
+        print()
+        user_input = input("Enter the index of the next children to navigate to, or enter '-' to go to the parent node (enter 'exit' to quit): ") 
+    
+    # Conditionals that act on what to do given the user input
+    if user_input.lower() == 'exit':
+        # User said exit
+        break
+    elif user_input.lower() == '-': 
+        # User said visit parent node: we just delete the last char of the node_id
+        if len(current_node_id)>1: # But only if we are not at the root
+            current_node_id = current_node_id[:-1]
+        else:
             print()
             print('---------------------------------------')
-            print(f"Current node ID is '{current_node_id}'. These are its children and their songs:")
-            for i in range(current_node['n_children']):
-                
-                print()
-                if current_node['children'][i]['is_leaf']:
-                    print(f"{i}: Child {i} (leaf)")
-                else:
-                    print(f"{i}: Child {i}")
-                
-                # And let's print the songs of each children
-                child_samples = pd.DataFrame.from_dict(current_node['children'][i]['samples'], orient='index')
-                print(f"With {len(child_samples)} songs, among which:")
-                test_utils.print_songs_from_df(child_samples)
-
-            # Get user input for the next node to navigate
-            print()
-            user_input = input("Enter the index of the next children to navigate to, or enter '-' to go to the parent node (enter 'exit' to quit): ") 
-        
-        # Conditionals that act on what to do given the user input
-        if user_input.lower() == 'exit':
-            # User said exit
-            break
-        elif user_input.lower() == '-': 
-            # User said visit parent node: we just delete the last char of the node_id
-            if len(current_node_id)>1: # But only if we are not at the root
-                current_node_id = current_node_id[:-1]
-            else:
-                print()
-                print('---------------------------------------')
-                print("! You are already at the root node, there is no parent to navigate to.")
-        elif user_input.isdigit(): 
-            # User gave a number, we visit the following node
-            if not current_node['is_leaf']:
-                # Update the node_id and continue
-                current_node_id += user_input
-            
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        break
+            print("! You are already at the root node, there is no parent to navigate to.")
+    elif user_input.isdigit(): 
+        # User gave a number, we visit the following node
+        if not current_node['is_leaf']:
+            # Update the node_id and continue
+            current_node_id += user_input
 
 node = current_node
--- src/model/test/python/test_utils.py
+++ src/model/test/python/test_utils.py
@@ -77,5 +77,5 @@ if running_in_docker():
     # If running in Docker, use the environment variable
     TESTS_PATH = load_env_var('PYTHON_TESTS_PATH')
 else: # Not running in Docker, use static paths
-    TESTS_PATH = '.'
+    TESTS_PATH = './src/model/test/python'
     make_test_results_folder("./results")
