34619eaa98c95f072870bc0601915337214efb1d
nicoloddo <gnicoloddo@gmail.com>
2024-11-06 16:25:27 +0100

    Added StatusController to print information on screen

---
+++ src/unity/portamento_web/Assets/Scripts/UI/StatusController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusController : MonoBehaviour
+{
+    public Text StatusLabel;
+    public float clearDelay = 3f; // Time in seconds before clearing
+    public bool manualMode = true; // Toggle for manual mode
+    
+    private Coroutine clearCoroutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StatusLabel.text = "Give us a sec... We are loading the multiverse!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetStatus(string status)
+    {
+        StatusLabel.text = status;
+        
+        if (!manualMode && clearCoroutine != null)
+        {
+            StopCoroutine(clearCoroutine);
+        }
+        
+        if (!manualMode)
+        {
+            clearCoroutine = StartCoroutine(ClearStatusAfterDelay());
+        }
+    }
+
+    private IEnumerator ClearStatusAfterDelay()
+    {
+        yield return new WaitForSeconds(clearDelay);
+        StatusLabel.text = "";
+        clearCoroutine = null;
+    }
+}
