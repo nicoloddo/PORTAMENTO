d318c1ba09e3cfaf212f3af2104739de2bf54ff3
nicoloddo <gnicoloddo@gmail.com>
2024-11-09 16:30:55 +0100

    The ui now does not tilt when hidden, possibly weighting less on computation time

--- src/unity/portamento_web/Assets/Scripts/UI/MenuHider.cs
+++ src/unity/portamento_web/Assets/Scripts/UI/MenuHider.cs
@@ -6,16 +6,18 @@ using UnityEngine.EventSystems;
 public class MenuHider : MonoBehaviour
 {
     [SerializeField] private GameObject[] _menuWindows;  // Drag your window objects here in the Inspector
-    private bool _isActive = true;
-
-    public bool IsActive
-    {
-        get { return _isActive; }
-    }
+    public bool IsActive = true;
 
     public void SetActive(bool active)
     {
-        _isActive = active;
+        IsActive = active;
         gameObject.GetComponent<Animator>().SetBool("Open", active);
+
+        // Disable/enable TiltWindow components on this object and all children
+        TiltWindow[] tiltWindows = gameObject.GetComponentsInChildren<TiltWindow>(true);
+        foreach (TiltWindow tiltWindow in tiltWindows)
+        {
+            tiltWindow.enabled = active;
+        }
     }
 }
